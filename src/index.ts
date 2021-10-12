71ab8bef01d4756cbc068f16c6e634da9735c895
张潇 <wszxdhr@126.com>
2021-10-12 22:49:38 +0800

    chore: 配置开发环境

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
 export * from './lib/async';
 export * from './lib/number';
+import Koa from 'koa';
+
+
+console.log('hello world!!');
+const app = new Koa();
+
+app.use(async () => {
+  return undefined;
+});
+
+app.listen(3000);
