353e8e888d24b64b1c2caf7fd783b185f640ac52
张潇 <wszxdhr@126.com>
2021-10-13 23:31:28 +0800

    feat: 增加axios请求

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 export * from './lib/async';
 export * from './lib/number';
-import Koa from 'koa';
+import axios from 'axios';
+import Koa, { Context } from 'koa';
+import qs from 'qs';
+
+import Car from './configs/car.json';
 
 console.log('hello world!!');
 const app = new Koa();
 
-app.use(async () => {
-  return undefined;
+app.use(async (ctx: Context, next: () => Promise<void>) => {
+  ctx.body = (await axios.post('https://app-connected.gtmc.com.cn/appcarnetwork/app/getVehicleStatus', qs.stringify(Car.reqBody), {
+    headers: Car.headers
+  })).data;
+  console.log(ctx.body);
+  await next();
 });
 
 app.listen(3000);
